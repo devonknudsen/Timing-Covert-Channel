584c1e8b32fda7e0bc76d7571f28f32bc4164ea3
ZackaryPhillips <37816812+ZackaryPhillips@users.noreply.github.com>
2019-10-08 21:51:27 -0500

    Update client.py

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ from time import time
 ip = "localhost"
 port = 1337
 ONE = 0.1
-ZERP = 0.025
+ZERO= 0.025
 covert_bin = ""
 
 # create the socker
@@ -16,23 +16,29 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ip, port))
 
 # recieve data until EOF
-data = s.recv(4096)
+data = s.recv(4096).decode().rstrip("\n")
 
 print("Received characters with delays:")
-while (data.rstrip("\n") != "EOF"):
-    stdout.write(data)
+
+while (data != "EOF"):
+    stdout.write(str(data))
     stdout.flush()
     t0 = time()
-    data = s.recv(4096)
+    data = s.recv(4096).decode().rstrip("\n")
     t1 = time()
     delta = round(t1 - t0, 3)
-    if (delta < ONE):
-        print("\tZero: \t" + str(delta))
-        covert_bin += "0"
-    else:
+    if (delta > ZERO):
         print("\tOne: \t" + str(delta))
         covert_bin += "1"
+    else:
+        print("\tZero: \t" + str(delta))
+        covert_bin += "0"
+
 print("Binary received: " + str(covert_bin))
+question = input("Do you wish to show hidden msg? Y/N\n")
+if (question.upper() == 'N'):
+    print("Goodbye")
+    exit()
 print("\nConvert 8 byte binary to character:")
 # close the connection to the server
 s.close()
@@ -52,4 +58,3 @@ while (i < len(covert_bin)):
     i += 8
     print("")
 print("Covert message: " + covert)
-
