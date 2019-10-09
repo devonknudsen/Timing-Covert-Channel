e24efdf12c3eac5b7b05d0ed37f03176095d08f9
ZackaryPhillips <37816812+ZackaryPhillips@users.noreply.github.com>
2019-10-08 21:51:51 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,20 +2,21 @@ import socket
 from time import sleep
 from binascii import hexlify
 
-port = 1337
+ZERO = 0.025
+ONE = 0.1
+PORT = 1337
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("", port))
+s.bind(("", PORT))
 
 
-covert = "secret" + "EOF"
+covert = "THIS IS A SECRET THING " + "EOF"
 print("Covert message: " + covert)
 covert_bin = ""
 for i in covert:
-    covert_bin += bin(int(hexlify(i), 16))[2:].zfill(8)
-print("Covert in binary: " + str(covert_bin))
+    covert_bin += bin(int(hexlify(i.encode()), 16))[2:].zfill(8)
+    
+print("Covert in binary: " + covert_bin)
 n = 0
-zero = 0.025
-one = 0.1
 bin = ""
 
 # listens for clients
@@ -25,27 +26,24 @@ s.listen(0)
 # a client has connected!
 c,addr = s.accept()
 
-
 # set the message
 msg = "Some message..."
 
 # send the message, one letter at a time
 print("Sending characters with delays:")
-for i in range(len(covert)):
-    j = i
-    if j > len(msg):
-        j = i - len(msg)
-    c.send(msg[j])
-    if(covert_bin[n] == "0"):
-        sleep(zero)
-        print("Zero: \t" + str(zero))
-        bin += "0"
-    else:
-        sleep(one)
-        print("One: \t" + str(one))
-        bin += "1"
-    n = (n+1)%len(covert_bin)
-
-c.send("EOF")
+n = 0
+count = 0
+while(count < len(covert_bin)):
+    for i in msg:
+        c.send(i.encode())
+        if (covert_bin[n] == "0"):
+            sleep(ZERO)
+        else:
+            sleep(ONE)
+        n = (n + 1) % len(covert_bin)
+        count += 1
+        
+c.send("EOF".encode())
 c.close()
-print("Binary sent: " + str(bin))
+
+
