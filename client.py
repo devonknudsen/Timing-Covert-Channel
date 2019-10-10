022b6f2ae01f623ca9c4b26050b0628689b843c0
devonknudsen <37875536+devonknudsen@users.noreply.github.com>
2019-10-09 21:03:52 -0500

    My version of the client

--- client.py
+++ client.py
@@ -2,59 +2,73 @@ import socket
 from sys import stdout
 from time import time
 
+DEBUG = True
+
 # set the server's IP address and port
-ip = "localhost"
-port = 1337
+IP = "localhost"
+PORT = 1337
+
+# set time interval to binary equivalent
 ONE = 0.1
-ZERO= 0.025
+ZERO = 0.025
 covert_bin = ""
 
-# create the socker
+# create the socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect to server
-s.connect((ip, port))
+s.connect((IP, PORT))
+
+# recieve initial piece of data
+data = s.recv(4096).decode()
 
 # recieve data until EOF
-data = s.recv(4096).decode().rstrip("\n")
-
-print("Received characters with delays:")
-
-while (data != "EOF"):
-    stdout.write(str(data))
+while (data.rstrip("\n") != "EOF"):
+    stdout.write(data)
     stdout.flush()
     t0 = time()
-    data = s.recv(4096).decode().rstrip("\n")
+    data = s.recv(4096).decode()
     t1 = time()
     delta = round(t1 - t0, 3)
-    if (delta > ZERO):
-        print("\tOne: \t" + str(delta))
+    if (delta >= ONE):
+        if(DEBUG):
+            stdout.write("\tOne: \t" + str(delta) + "\n")
         covert_bin += "1"
     else:
-        print("\tZero: \t" + str(delta))
+        if(DEBUG):
+            stdout.write("\tZero: \t" + str(delta) + "\n")
         covert_bin += "0"
+    
+    stdout.flush()
 
-print("Binary received: " + str(covert_bin))
-#question = input("Do you wish to show hidden msg? Y/N\n")
-#if (question.upper() == 'N'):
-#    print("Goodbye")
- #   exit()
-print("\nConvert 8 byte binary to character:")
 # close the connection to the server
 s.close()
+
+if(DEBUG):
+    print("Binary received: " + str(covert_bin))
+    print("\nConvert 8 byte binary to character:")
 
 covert = ""
 i = 0
 while (i < len(covert_bin)):
     b = covert_bin[i:i+8]
-    print("bytes: \t" + str(b))
+
+    # break if there isn't an entire byte
+    if(len(b) != 8):
+        break
+
+    if(DEBUG):
+        print("bytes: \t" + str(b))
     n = int("0b{}".format(b),2)
-    print("int: " + str(n))
+    if(DEBUG):
+        print("int: " + str(n))
     try:
         covert += chr(n)
-        print("char: \t" + chr(n))
+        if(DEBUG):
+            print("char: \t" + chr(n))
     except:
         covert += "?"
+        
     i += 8
-    print("")
+
 print("Covert message: " + covert)
