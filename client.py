0491d1bcf2d51f93f1bd1c328425f6842675b9d5
devonknudsen <37875536+devonknudsen@users.noreply.github.com>
2019-10-10 19:35:55 -0500

    Newly updated version

--- client.py
+++ client.py
@@ -11,7 +11,6 @@ PORT = 1337
 # set time interval to binary equivalent
 ONE = 0.1
 ZERO = 0.025
-covert_bin = ""
 
 # create the socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -23,6 +22,7 @@ s.connect((IP, PORT))
 data = s.recv(4096).decode()
 
 # recieve data until EOF
+covert_bin = ""
 while (data.rstrip("\n") != "EOF"):
     stdout.write(data)
     stdout.flush()
@@ -30,16 +30,17 @@ while (data.rstrip("\n") != "EOF"):
     data = s.recv(4096).decode()
     t1 = time()
     delta = round(t1 - t0, 3)
+    
+    # display times between letters
+    if(DEBUG):
+        stdout.write("\tTime: \t" + str(delta) + "\n")
+        stdout.flush()
+        
     if (delta >= ONE):
-        if(DEBUG):
-            stdout.write("\tOne: \t" + str(delta) + "\n")
         covert_bin += "1"
     else:
-        if(DEBUG):
-            stdout.write("\tZero: \t" + str(delta) + "\n")
         covert_bin += "0"
     
-    stdout.flush()
 
 # close the connection to the server
 s.close()
@@ -53,22 +54,21 @@ i = 0
 while (i < len(covert_bin)):
     b = covert_bin[i:i+8]
 
-    # break if there isn't an entire byte
+    # break if there isn't an entire byte left
     if(len(b) != 8):
-        break
-
-    if(DEBUG):
-        print("bytes: \t" + str(b))
+        break   
     n = int("0b{}".format(b),2)
-    if(DEBUG):
-        print("int: " + str(n))
     try:
-        covert += chr(n)
         if(DEBUG):
-            print("char: \t" + chr(n))
+            print("byte:\t" + str(b))
+            print("int conversion: " + str(n))
+            print("char conversion:\t" + chr(n) + "\n")
+            
+        covert += chr(n)
+        
     except:
         covert += "?"
         
     i += 8
 
-print("Covert message: " + covert)
+print("\nCovert message: " + covert)
